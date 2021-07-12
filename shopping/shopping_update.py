2fc1b762ff137b4db99a7e4bde501fc53cf545f9
Sven <glaglasven@live.com>
2021-07-12 14:28:12 +0200

    Added uptimum racing

--- shopping/shopping_update.py
+++ shopping/shopping_update.py
@@ -50,7 +50,52 @@ def load_helifree():
                 )
 
 
+def load_optimum_racing():
+    urls = [
+        ["https://optimum-racing.ch/fr/cameras-fpv-numerique-hd/833-caddx-vista-air-unit-lite-la-vista-a-ete-entierement-autorise-par-dji-et-compatible-avec-leur-systeme-hd-fpv-encore-mieux-la-cad.html", "Caddx Vista"],
+        ["https://optimum-racing.ch/fr/cameras-fpv-numerique-hd/1292-nebula-pro-vista-kit-nebula-pro-vista-kit-720p-120fps-systeme-fpv-numerique-hd-a-faible-latence-14990-chf.html", "Caddx Nebula Pro"]
+    ]
+    # Caddx air unit
+    for url in urls:
+        page = urllib.request.urlopen(url[0])
+        page_content = str(page.read())
+        if "pas assez de produits en stock" in page_content:
+            content.append(
+                {
+                    "shop": "Optimum Racing",
+                    "item": url[1],
+                    "availability": "0",
+                    "link": url[0],
+                    "time": datetime.datetime.now().strftime("%c")
+                }
+            )
+        else:
+            # Look or <span data-stock="x"
+            x = re.findall("data-stock=.{3}", page_content)
+            if len(x) > 0:
+                content.append(
+                    {
+                        "shop": "Optimum Racing",
+                        "item": url[1],
+                        "availability": x[0].split("\"")[1],
+                        "link": url[0],
+                        "time": datetime.datetime.now().strftime("%c")
+                    }
+                )
+            else:
+                content.append(
+                    {
+                        "shop": "Optimum Racing",
+                        "item": url[1],
+                        "availability": "unknown",
+                        "link": url[0],
+                        "time": datetime.datetime.now().strftime("%c")
+                    }
+                )
+
+
 load_helifree()
+load_optimum_racing()
 
 f = open("shopping.json", "w")
 f.write(json.dumps(content))
